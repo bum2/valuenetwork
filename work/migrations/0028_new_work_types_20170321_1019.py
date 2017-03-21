114af5b6520114f4ecb7c1f13eb73dfb95f54b91
pospi <pospi@spadgos.com>
2017-03-21 14:49:37 +0200

    insert data from facets2.sql as well

--- work/migrations/0028_new_work_types_20170321_1019.py
+++ work/migrations/0028_new_work_types_20170321_1019.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 from work.models import Ocp_Artwork_Type, Ocp_Skill_Type, Ocp_Unit_Type, Ocp_Record_Type
+from valueaccounting.models import Facet, FacetValue
 
 # helper for performing checks & inserts
 
@@ -18,6 +19,56 @@ def ensureRows(model, keys, rows):
             print "created new %s record (id %d)" % (model.__name__, row[0])
 
 # data inserts
+
+def insertFacects(apps, schema_editor):
+    keys = (
+        "id",
+        "name",
+        "description",
+        "clas"
+    )
+
+    rows = (
+        (1,'Material','This is a set of material resource types or facet values, in coordination with the General common tree of Material Artwork Types (physical items). This facet relates a General model class which manages the subtypes as a tree.','Material_Type'),
+        (2,'Types of Work','This is a set of Skill resource types or facet values, in coordination with the General common tree of Jobs (arts, verbs). This facet relates a Work model class which manages the subtypes as a tree.','Skill_Type'),
+        (3,'Non-material','This is a set of non-material resource types or facet values, in coordination with the General common tree of Non-material Artwork Types (physical items). This facet relates a General model class which manages the subtypes as a tree.','Nonmaterial_Type'),
+        (4,'Currency','This facet is to group types of currencies, so a resource type can act as a currency of certain type if wears any of this values','Currency_Type'),
+    )
+
+    ensureRows(Facet, keys, rows)
+
+def insertFacetValues(apps, schema_editor):
+    keys = (
+        "id",
+        "value",
+        "description",
+        "facet_id",
+    )
+
+    rows = (
+        (1,'Product','',1),
+        (8,'Space','',1),
+        (9,'Raw material','',1),
+        (10,'Tool','',1),
+        (12,'Making','',2),
+        (13,'Communicating','',2),
+        (14,'LifeCareing','',2),
+        (16,'Visual','',3),
+        (17,'Formation','',3),
+        (18,'Sound','',3),
+        (19,'Text','',3),
+        (21,'Food\'n\'Drinks','',1),
+        (22,'Digital','',3),
+        (23,'Service','',3),
+        (24,'Money','',3),
+        (25,'Transporting','',2),
+        (26,'Fair currency','',4),
+        (27,'Fiat currency','',4),
+        (28,'Social currency','',4),
+        (29,'Crypto currency','',4),
+    )
+
+    FacetValue(Facet, keys, rows)
 
 def insertArtworkTypes(apps, schema_editor):
     keys = (
@@ -284,6 +335,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(insertFacects),
+        migrations.RunPython(insertFacetValues),
+
         migrations.RunPython(insertEconomicResourceTypes),
         migrations.RunPython(insertArtworkTypes),
         migrations.RunPython(insertSkillTypes),
